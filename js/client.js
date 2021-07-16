2990861f218f38fa3e835b7494f9c3cda6771dfb
the-tushar-kumar <78680551+the-tushar-kumar@users.noreply.github.com>
2021-07-16 16:14:37 +0530

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,48 @@
+//jshint esversion: 6
+const socket = io('http://localhost:8000');
+
+// Get DOM elements in respective Js variables
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+
+// Audio that will play on receiving messages
+var audio = new Audio('ting.mp3');
+
+// Function which will append event info to the contaner
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position =='left'){
+        audio.play();
+    }
+};
+
+// If the form gets submitted, send server the message
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+});
+
+
+// Ask new user for his/her name and let the server know
+const name = prompt("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+// If a new user joins, receive his/her name from the server
+socket.on('user-joined', name =>{
+    append(`${name} joined the chat`, 'right');
+});
+
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left');
+});
+socket.on('left', name =>{
+    append(`${name} left the chat`, 'right');
+});
